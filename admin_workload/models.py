1578174f81cda804c9771f74a332ff5de290aac9
Profiliu <muaprof2@gmail.com>
2022-10-30 00:41:16 +0200

    Added Incomplete workload models - Admin Workload Model - Community Workload Model

--- admin_workload/models.py
+++ admin_workload/models.py
@@ -1,3 +1,15 @@
+import uuid
 from django.db import models
+from appauth.models import *
 
 # Create your models here.
+class AdminWorkload(models.Model):    
+    ActivityTypes = models.TextChoices('ActivityTypes', 'Compulsory Major Minor Voluntary')
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False) 
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    activity_type = models.CharField(choices=ActivityTypes.choices, max_length=12)
+    hours_per_semester = models.IntegerField()
+
+    def __str__(self):
+        return self.name
--- community_workload/models.py
+++ community_workload/models.py
@@ -1,4 +1,15 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+class CommunityWorkload(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False) 
+    activity_name = models.CharField(max_length=50)
+    # evidence = models.FileField()
+    # user = models.ManyToManyField()
+    hours_per_semester = models.IntegerField()
+
+
+    def __str__(self):
+        return self.activity_name
 
