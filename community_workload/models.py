189d7710dc70f33c9f25f8e6587350a35871279b
Profiliu <muaprof2@gmail.com>
2022-10-30 21:53:45 +0200

    Updated community engagement model - Added semester field

--- community_workload/models.py
+++ community_workload/models.py
@@ -3,10 +3,15 @@ from django.db import models
 
 # Create your models here.
 class CommunityWorkload(models.Model):
+    SemesterType = (
+        ('1st', 'First Semester'),
+        ('2nd', 'Second Semester')
+    )
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False) 
     activity_name = models.CharField(max_length=50)
     # evidence = models.FileField()
     # user = models.ManyToManyField()
+    semester = models.CharField(choices=SemesterType, max_length=20)
     hours_per_semester = models.IntegerField()
 
 
