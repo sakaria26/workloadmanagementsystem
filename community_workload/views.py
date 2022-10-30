ec1ea8dd4b12cc6dd792c9e6f674b24cb86c527f
Profiliu <muaprof2@gmail.com>
2022-10-30 21:54:59 +0200

    Added create community workload page

--- community_workload/views.py
+++ community_workload/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def all_community_workloads(request):
     return render(request, 'community_workload/community_workload.html', {})
+
+def add_community_workload(request):
+    return render(request, 'community_workload/add_community_load.html', {})
