59617325f191d31eba260ef43956fc396d7a664b
Sakaria Nghivafe <snghivafe@gmail.com>
2022-11-08 10:27:52 +0200

    Changed Url Import

--- app/urls.py
+++ app/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.static import serve
 from django.conf.urls import url
 from django.conf import settings
@@ -28,8 +28,8 @@ urlpatterns = [
     path('research_load/', include('research_workload.urls')),
     path('teaching_load/', include('teaching_workload.urls')),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
